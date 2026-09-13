0354d78d5d0dbf25481661482d332f4230c257f5
FernandoAdelinoSilva <fernandoadelinodasilva@hotmail.com>
2026-09-13 16:05:46 -0300

    feat: implement AccountService with business rules

--- Banking.Application/AccountService.cs
+++ Banking.Application/AccountService.cs
@@ -8,31 +8,43 @@ public class AccountService : IAccountService
 
     public Account CreateAccount()
     {
-        throw new NotImplementedException();
+        var account = new Account(Guid.NewGuid());
+        _accounts[account.Id] = account;
+        return account;
     }
 
     public void Deposit(Guid accountId, decimal amount)
     {
-        throw new NotImplementedException();
+        var account = GetAccount(accountId);
+        account.Deposit(amount);
     }
 
     public void Withdraw(Guid accountId, decimal amount)
     {
-        throw new NotImplementedException();
+        var account = GetAccount(accountId);
+        account.Withdraw(amount);
     }
 
     public void Transfer(Guid fromAccountId, Guid toAccountId, decimal amount)
     {
-        throw new NotImplementedException();
+        var fromAccount = GetAccount(fromAccountId);
+        var toAccount = GetAccount(toAccountId);
+
+        fromAccount.Withdraw(amount);
+        toAccount.Deposit(amount);
     }
 
     public decimal GetBalance(Guid accountId)
     {
-        throw new NotImplementedException();
+        var account = GetAccount(accountId);
+        return account.Balance;
     }
 
     private Account GetAccount(Guid accountId)
     {
-        throw new NotImplementedException();
+        if (!_accounts.TryGetValue(accountId, out var account))
+            throw new InvalidOperationException("Account not found.");
+
+        return account;
     }
 }
