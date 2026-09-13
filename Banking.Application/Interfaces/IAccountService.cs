976625da013888f197682b3eeb2b5152c50f3b7e
FernandoAdelinoSilva <fernandoadelinodasilva@hotmail.com>
2026-09-13 14:43:44 -0300

    feat: define AccountService with signature methods

---
+++ Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Banking.Domain;
+
+namespace Banking.Application.Interfaces;
+public interface IAccountService
+{
+    Account CreateAccount();
+    void Deposit(Guid accountId, decimal amount);
+    void Withdraw(Guid accountId, decimal amount);
+    void Transfer(Guid fromAccountId, Guid toAccountId, decimal amount);
+    decimal GetBalance(Guid accountId);
+}
