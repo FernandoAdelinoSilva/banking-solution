073697774a660dc490a20fdab3cc8480aa1096df
FernandoAdelinoSilva <fernandoadelinodasilva@hotmail.com>
2026-09-13 13:45:35 -0300

    feat: add account entity to domain

---
+++ Banking.Domain/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace Banking.Domain;
+public class Account
+{
+    public Guid Id { get; private set; }
+    public decimal Balance { get; private set; }
+
+    public Account(Guid id)
+    {
+        Id = id;
+        Balance = 0m;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Deposit amount must be positive.");
+
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Withdraw amount must be positive.");
+
+        if (Balance < amount)
+            throw new InvalidOperationException("Insufficient funds.");
+
+        Balance -= amount;
+    }
+}
